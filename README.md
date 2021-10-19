b00fcb09945d09bfb8d38ae2adf0ea30e5e9ad74
mikeyxx <michaelogboo@gmail.com>
2021-10-19 06:29:09 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+- [Overview](#overview)
+
+
+## Overview
+
+Working on this project was very challenging for me as this is my first time of working with JavaScript on a project. I've only previously built projects with HTML and CSS. Although I had a lot of help working on this project, I am really proud that I was able to complete it. I'm proud of how great it looks. I will be taking on more JavaScript projects to help me improve.
+
+Please check it out and let me know what you think.
+
+
---
+++ script.js
@@ -0,0 +1,96 @@
+document.querySelector('.reset-btn').style.textTransform = "uppercase";
+const bill = document.querySelector('.bill-input');
+const people = document.querySelector('.people-input');
+const btn5 = document.querySelector('.five-percent');
+const btn10 = document.querySelector('.ten-percent');
+const btn15 = document.querySelector('.fifteen-percent');
+const btn25 = document.querySelector('.twenty-five-percent');
+const btn50 = document.querySelector('.fifty-percent');
+const custom = document.querySelector('.custom-btn');
+
+const person = document.querySelector('.tip-person');
+const T_people = document.querySelector('.tip-total');
+const peopleContainer = document.querySelector('.input-people');
+const inputContainer = document.querySelector('.input-bill');
+
+const reset = document.querySelector('.reset-btn');
+
+let Regex = /^[0-9]/;
+
+let billValue = 0;
+let billAmount = function() {
+    bill.addEventListener('keyup', function() {
+        billValue = bill.value;
+        return billValue
+    })
+}
+billValue = billAmount();
+
+let peopleValue = 0;
+let peopleAmount = function() {
+    people.addEventListener('keyup', function() {
+        peopleValue = people.value;
+        return peopleValue
+    })
+}
+peopleValue = peopleAmount();
+
+
+let tipAmount = 0;
+let totalPerson = 0;
+
+function calculateTip(value) {
+    if (!peopleValue || Regex.test(peopleValue) == false) {
+        peopleContainer.classList.add('invalid');
+    } else {
+        peopleContainer.classList.remove('invalid');
+    }
+
+    if (Regex.test(billValue) == false) {
+        inputContainer.classList.add('invalid');
+    } else {
+        inputContainer.classList.remove('invalid');
+    }
+    tipAmount = (billValue * value) / peopleValue;
+    tipAmount = Math.round(tipAmount)
+    console.log(tipAmount);
+    person.textContent = `$${tipAmount}`;
+
+    totalPerson = (billValue / peopleValue) + tipAmount;
+    totalPerson = Math.round(totalPerson);
+    T_people.textContent = `$${totalPerson}`;
+}
+
+btn5.addEventListener('click', () => {
+        calculateTip(0.05);
+    });
+btn10.addEventListener('click', () => {
+        calculateTip(0.1);
+    });
+btn15.addEventListener('click', () => {
+        calculateTip(0.15);
+    });
+btn25.addEventListener('click', () => {
+        calculateTip(0.25);
+    });
+btn50.addEventListener('click', () => {
+        calculateTip(0.5);
+    });
+
+let customValue = 0;
+custom.addEventListener('keyup', function() {
+    customValue = custom.value;
+    calculateTip(customValue / 100);
+})
+
+
+reset.addEventListener('click', () => {
+    person.textContent = `$0.00`;
+    T_people.textContent = `$0.00`;
+    bill.value = null;
+    people.value = null;
+    custom.value = null;
+    peopleContainer.classList.remove('invalid');
+});
+
+
